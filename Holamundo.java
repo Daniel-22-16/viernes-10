743e7cf821af5ecf8e064253bc7878786d070f61
Daniel-22-16 <1475937@senati.pe>
2023-03-10 07:47:42 -0500

    viernes10

---
+++ Holamundo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Holamundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World SENATI");
+        System.out.println("Carlos Daniel");
+        System.out.println("Puente Aponte");
+    }
+}
